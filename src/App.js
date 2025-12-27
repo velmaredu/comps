a10689931560fdcac37e64af664aed6487766270
eevm <eevm@gmv.com>
2025-12-27 21:32:39 +0100

    Section 10 - Button component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
+import Button from './components/Button';
 
 function App() {
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>React Starter Project</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.js</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    const handleClick = () => {
+        console.log('Button clicked!');
+    }
+
+    return (
+        <div>
+            <div>
+                <Button success rounded outline onClick={handleClick}>
+                    <GoBell />
+                    Click me!!
+                </Button>
+            </div>
+            <div>
+                <Button danger outline>
+                    <GoCloudDownload />
+                    Buy Now!
+                </Button>
+            </div>
+            <div>
+                <Button warning>
+                    <GoDatabase />
+                    See Deal!
+                </Button>
+            </div>
+            <div>
+                <Button secondary outline>Hide Ads!</Button>
+            </div>
+            <div>
+                <Button primary rounded>Something!</Button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
